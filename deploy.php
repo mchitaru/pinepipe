298f714c8b3fbc5230a6302bf7c8d9d4e1da32f9
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2020-01-05 21:51:09 +0200

    deploy

--- deploy.php
+++ deploy.php
@@ -5,6 +5,7 @@ require 'recipe/laravel.php';
 
 // Project name
 set('application', 'BaseCRM');
+set('keep_releases', 5);
 
 // Project repository
 set('repository', 'https://github.com/mchitaru/basecrm.git');
