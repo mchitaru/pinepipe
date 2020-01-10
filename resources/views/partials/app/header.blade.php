193a6fd8e7f928daf2600b7c520635b2c9204749
corsair <marius.chitaru@gmail.com>
2020-01-10 09:08:00 +0200

    header menu

--- resources/views/partials/app/header.blade.php
+++ resources/views/partials/app/header.blade.php
@@ -25,6 +25,7 @@
 
     </li>
 
+    @if(\Auth::user()->type!='super admin' && Gate::check('manage client'))
     <li class="nav-item">
 
         <div class="dropdown">
@@ -35,23 +36,25 @@
 
                 <a class="dropdown-item" href="{{ route('clients.index') }}">Contacts</a>
 
+                @if(Gate::check('manage lead'))
                 <a class="dropdown-item" href="#">Leads</a>
+                @endif
 
                 <a class="dropdown-item" href="{{ route('clients.index') }}">Activity</a>
 
             </div>
         </div>
     </li>
+    @endif
 
+    @if(Gate::check('manage project'))
     <li class="nav-item">
 
         <div class="dropdown">
         <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-expanded="false" aria-haspopup="true" id="nav-dropdown-2">Projects</a>
         <div class="dropdown-menu">
 
-            @if(Gate::check('manage project'))
-                <a class="dropdown-item" href="{{ route('projects.index') }}">{{__('Projects')}}</a>
-            @endif
+            <a class="dropdown-item" href="{{ route('projects.index') }}">{{__('Projects')}}</a>
 
             <a class="dropdown-item" href="#">Project</a>
 
@@ -63,7 +66,9 @@
         </div>
 
     </li>
+    @endif
 
+    @if((Gate::check('manage product') || Gate::check('manage invoice') || Gate::check('manage expense') || Gate::check('manage payment') || Gate::check('manage tax')) || \Auth::user()->type=='client')
     <li class="nav-item">
 
         <div class="dropdown">
@@ -82,7 +87,9 @@
         </div>
 
     </li>
+    @endif
 
+    @if(Gate::check('manage project'))
     <li class="nav-item">
 
         <div class="dropdown">
@@ -95,6 +102,7 @@
         </div>
 
     </li>
+    @endif
 
     </ul>
     <div class="d-lg-flex align-items-center">
@@ -109,68 +117,71 @@
         </div>
         @endif
 
-    <div class="dropdown mx-lg-2">
-        <button class="btn btn-primary btn-block dropdown-toggle" type="button" id="newContentButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        Add New
-        </button>
-        <div class="dropdown-menu">
-        <a class="dropdown-item" href="#">Team</a>
-        <a class="dropdown-item" href="#">Project</a>
-        <a class="dropdown-item" href="#">Task</a>
+        @if(Gate::check('manage project'))
+        <div class="dropdown mx-lg-2">
+            <button class="btn btn-primary btn-block dropdown-toggle" type="button" id="newContentButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Add New
+            </button>
+            <div class="dropdown-menu">
+            <a class="dropdown-item" href="#">Team</a>
+            <a class="dropdown-item" href="#">Project</a>
+            <a class="dropdown-item" href="#">Task</a>
+            </div>
         </div>
-    </div>
-    @if(\Auth::user()->type!='client')
-    <!-- Settings menu --->
-    <div class="dropdown mx-lg-2">
-        <button class="btn btn-round" role="button" data-toggle="dropdown" aria-expanded="false">
-            <i class="material-icons">settings</i>
-        </button>
-        <div class="dropdown-menu dropdown-menu-right">
+        @endif
 
-            @if(\Auth::user()->type=='super admin' || Gate::check('manage user'))
-                @can('manage user')
-                <a class="dropdown-item" href="{{ route('users.index') }}">{{__('Users')}}</a>
+        @if(\Auth::user()->type!='client')
+        <!-- Settings menu --->
+        <div class="dropdown mx-lg-2">
+            <button class="btn btn-round" role="button" data-toggle="dropdown" aria-expanded="false">
+                <i class="material-icons">settings</i>
+            </button>
+            <div class="dropdown-menu dropdown-menu-right">
+
+                @if(\Auth::user()->type=='super admin' || Gate::check('manage user'))
+                    @can('manage user')
+                    <a class="dropdown-item" href="{{ route('users.index') }}">{{__('Users')}}</a>
+                    @endcan
+                @endif
+                @if(Gate::check('manage role'))
+                    <a class="dropdown-item" href="{{ route('roles.index') }}">{{__('User Roles')}}</a>
+                @endif
+                <div class="dropdown-divider"></div>
+                @if(Gate::check('manage plan'))
+                    <a href="{{ route('plans.index') }}" class="dropdown-item">
+                        <span class="title">{{__('Price Plans')}}</span>
+                    </a>
+                @endif
+                @if(Gate::check('manage order'))
+                    <a href="{{ route('order.index') }}" class="dropdown-item">
+                        <span class="title">{{__('Orders')}}</span>
+                    </a>
+                @endif
+            </div>
+        </div>
+        @endif
+        <!-- Profile menu --->
+        <div class="dropdown mx-lg-2">
+            <div class="dropdown dropdown-toggle">
+            <a href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <img alt="{{$users->name}}" title="{{$users->name}}" src="{{(!empty($users->avatar)? $profile.'/'.$users->avatar : $profile.'/avatar.png')}}" class="avatar" />
+            </a>
+            <div class="dropdown-menu dropdown-menu-right">
+                @can('manage account')
+                <a href="{{route('profile',$users->id)}}" class="dropdown-item">
+                    {{__('Profile')}}
+                </a>
                 @endcan
-            @endif
-            @if(Gate::check('manage role'))
-                <a class="dropdown-item" href="{{ route('roles.index') }}">{{__('User Roles')}}</a>
-            @endif
-            <div class="dropdown-divider"></div>
-            @if(Gate::check('manage plan'))
-                <a href="{{ route('plans.index') }}" class="dropdown-item">
-                    <span class="title">{{__('Price Plans')}}</span>
+                <div class="dropdown-divider"></div>
+                <a href="{{ route('logout') }}" class="dropdown-item" onclick="event.preventDefault(); document.getElementById('frm-logout').submit();">
+                    {{__('Logout')}}
                 </a>
-            @endif
-            @if(Gate::check('manage order'))
-                <a href="{{ route('order.index') }}" class="dropdown-item">
-                    <span class="title">{{__('Orders')}}</span>
-                </a>
-            @endif
+                <form id="frm-logout" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    {{ csrf_field() }}
+                </form>
+            </div>
+            </div>
         </div>
-    </div>
-    @endif
-    <!-- Profile menu --->
-    <div class="dropdown mx-lg-2">
-        <div class="dropdown dropdown-toggle">
-        <a href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <img alt="{{$users->name}}" title="{{$users->name}}" src="{{(!empty($users->avatar)? $profile.'/'.$users->avatar : $profile.'/avatar.png')}}" class="avatar" />
-        </a>
-        <div class="dropdown-menu dropdown-menu-right">
-            @can('manage account')
-            <a href="{{route('profile',$users->id)}}" class="dropdown-item">
-                {{__('Profile')}}
-            </a>
-            @endcan
-            <div class="dropdown-divider"></div>
-            <a href="{{ route('logout') }}" class="dropdown-item" onclick="event.preventDefault(); document.getElementById('frm-logout').submit();">
-                {{__('Logout')}}
-            </a>
-            <form id="frm-logout" action="{{ route('logout') }}" method="POST" style="display: none;">
-                {{ csrf_field() }}
-            </form>
-        </div>
-        </div>
-    </div>
     </div>
 </div>
 </div>
