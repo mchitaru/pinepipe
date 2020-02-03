4ce6f5e42e41d5f2360195331e9e1c5c147da492
corsair <marius.chitaru@gmail.com>
2020-02-03 09:57:10 +0200

    optimization

--- resources/views/partials/app/footer.blade.php
+++ resources/views/partials/app/footer.blade.php
@@ -1,4 +1,6 @@
 
+<script type="text/javascript" src="{{ asset('js/manifest.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/vendor.js') }}"></script>
 <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
 
 <!-- Required vendor scripts (Do not remove) -->
