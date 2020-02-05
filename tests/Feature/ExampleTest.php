ce8c8f67f1ca566dc0411dd11234831c922d7a7b
corsair <marius.chitaru@gmail.com>
2020-02-05 09:14:03 +0200

    optimization

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,11 +2,14 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    // use WithoutMiddleware;
+
     /**
      * A basic test example.
      *
