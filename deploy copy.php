d18d8653c1ab8c64d0a943190181bb50e222add5
corsair <marius.chitaru@gmail.com>
2020-02-27 19:53:23 +0200

    deployer

---
+++ deploy copy.php
@@ -0,0 +1,46 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Project name
+set('application', 'BaseCRM');
+set('writable_mode', 'chown');
+set('http_user', 'baseqigg');
+set('keep_releases', '1');
+
+// Project repository
+set('repository', 'git@github.com:mchitaru/basecrm.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true);
+
+// Shared files/dirs between deploys
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server
+add('writable_dirs', []);
+
+
+// Hosts
+
+host('basecrm.io')
+    ->set('deploy_path', '/home/baseqigg/deployment');
+
+host('basecrm.ro')
+    ->set('deploy_path', '/home4/r76126base/deployment');
+
+// Tasks
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+before('deploy:symlink', 'artisan:migrate');
+
