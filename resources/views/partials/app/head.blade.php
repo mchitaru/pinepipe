0e0f6c1cb247f569abf0aa1a8b55fa1dae37a530
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2020-03-04 17:49:50 +0200

    page title

--- resources/views/partials/app/head.blade.php
+++ resources/views/partials/app/head.blade.php
@@ -5,13 +5,13 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ 'BaseCRM — CRM and Project Management made simple' }}</title>
+    <title>{{ 'BaseCRM — CRM and Project Management, made simple' }}</title>
 
-    <meta name="description" content="A simple project management platform">
+    <meta name="description" content="A simple CRM and Project Management platform, for freelancers and small businesses.">
     <link href="{{ asset('favicon.ico') }}" rel="icon" type="image/x-icon">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Gothic+A1" rel="stylesheet">
-    
+
     <!-- App css (keep last) -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" media="all" />
 
