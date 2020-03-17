f915b373758539f80736a63b1d0fdecbfc2215b8
corsair <marius.chitaru@gmail.com>
2020-03-17 09:18:37 +0200

    fix seeder

--- database/seeds/CompanySeeder.php
+++ database/seeds/CompanySeeder.php
@@ -41,8 +41,8 @@ class CompanySeeder extends Seeder
             $role = Role::findByName('client');
             $user->type = 'client';
             $user->created_by = $company->id;
-            $user->client_id = $faker->numberBetween(User::$SEED_COMPANY_ID + 1, 
-                                                    User::$SEED_COMPANY_ID + Client::$SEED);
+            $user->client_id = $faker->numberBetween((User::$SEED_COMPANY_IDX-1)*Client::$SEED + 1,
+                                                        User::$SEED_COMPANY_IDX*Client::$SEED);
             $user->save();
             $user->assignRole($role);
         });
