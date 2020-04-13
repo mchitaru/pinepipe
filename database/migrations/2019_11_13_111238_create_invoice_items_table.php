9cc2706512d5bed82dc4e1b9a000f97c3277fbae
corsair <marius.chitaru@gmail.com>
2020-04-13 14:07:55 +0300

    create invoice items table

--- database/migrations/2019_11_13_111238_create_invoice_items_table.php
+++ database/migrations/2019_11_13_111238_create_invoice_items_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateInvoiceProductsTable extends Migration
+class CreateInvoiceItemsTable extends Migration
 {
     /**
      * Run the migrations.
