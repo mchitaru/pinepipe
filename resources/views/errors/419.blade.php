53b1aeb3e7792a89e9762011b81b30296c689d0e
corsair <marius.chitaru@gmail.com>
2020-04-22 10:32:39 +0300

    session expired

---
+++ resources/views/errors/419.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.auth')
+
+@push('stylesheets')
+@endpush
+
+@push('scripts')
+@endpush
+
+@section('breadcrumb')
+@endsection
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-xl-5 col-lg-6 col-md-7">
+        <div class="text-center">
+            <h1 class="display-1 text-primary">419</h1>
+            <p> {{__('Because you were idle for some time, the page expired for security reasons.')}} </p>
+            <p>
+                <a href="{{route('login')}}">{{__('Click here to go to the login page')}}</a>
+             {{__('or ')}}
+             <a href="{{ route('home')}}">{{__('Contact Us')}}</a>
+              {{__('if you think this might be an error.')}}
+            </p>
+        </div>
+        </div>
+    </div>
+</div>
+@endsection
