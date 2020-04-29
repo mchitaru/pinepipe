02096315e7a91017ea05c3e65bf71f10a380a169
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2020-04-29 14:42:56 +0300

    nested categories

--- app/Category.php
+++ app/Category.php
@@ -5,9 +5,12 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 use Iatstuti\Database\Support\NullableFields;
+use App\Traits\Categorizable;
 
 class Category extends Model
 {
+    use Categorizable;
+
     protected $fillable = [
         'name',
         'active',
@@ -27,6 +30,21 @@ class Category extends Model
     public function expenses()
     {
         return $this->morphedByMany('App\Expense', 'categorizable');
+    }
+
+    public function payments()
+    {
+        return $this->morphedByMany('App\Payment', 'categorizable');
+    }
+
+    public function leads()
+    {
+        return $this->morphedByMany('App\Lead', 'categorizable');
+    }
+
+    public function children()
+    {
+        return $this->morphedByMany('App\Category', 'categorizable');
     }
 
     /**
