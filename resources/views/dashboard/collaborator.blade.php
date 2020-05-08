7d610c14553a43ca4974972c92d27e2670416e82
corsair <marius.chitaru@gmail.com>
2020-05-08 10:37:22 +0300

    dash padding

--- resources/views/dashboard/collaborator.blade.php
+++ resources/views/dashboard/collaborator.blade.php
@@ -23,16 +23,16 @@ use Carbon\Carbon;
 <div class="container">
     <div class="row justify-content-center">
         <div class="container-fluid">
-            <div class="row pt-3">
+            <div class="row pt-4">
                 <div class="col">
                     <h3> {{__('Hey')}} {{\Auth::user()->name}}! {{__('After')}} &#x2615;, {{__("here is what's coming up")}}:</h3>
-                    <div class="row pb-3">
+                    <div class="row pt-1 pb-3">
                         <x-upcoming title="TODAY" :tasks='$todayTasks' :events='$todayEvents'></x-upcoming>
                         <x-upcoming title="THIS WEEK" :tasks='$thisWeekTasks' :events='$thisWeekEvents'></x-upcoming>
                         <x-upcoming title="NEXT WEEK" :tasks='$nextWeekTasks' :events='$nextWeekEvents'></x-upcoming>
                     </div>
                     <h3> {{__("Let's take on the day!")}} </h3>
-                    <div class="row pb-3">
+                    <div class="row pt-1 pb-3">
                         <x-todo type="projects" icon="folder" :text="__('project(s) in progress.')" :items='$projects'></x-todo>
                         <x-todo type="tasks" icon="playlist_add_check" :text="__('important thing(s) to do.')" :items='$tasks'></x-todo>
                         <x-todo type="leads" icon="phone" :text="__('lead(s) rotting.')" :items='$leads'></x-todo>
