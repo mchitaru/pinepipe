30637bcbcff93df7f4e7011c0aa9348a92c1d4ca
corsair <marius.chitaru@gmail.com>
2020-06-03 20:17:24 +0300

    fix translations

--- app/Http/Controllers/SubscriptionPlansController.php
+++ app/Http/Controllers/SubscriptionPlansController.php
@@ -41,32 +41,19 @@ class SubscriptionPlansController extends Controller
     {
         if(\Auth::user()->type == 'super admin')
         {
-            if(empty(env('STRIPE_KEY')) || empty(env('STRIPE_SECRET')))
-            {
-                return redirect()->back()->with('error', __('Please set stripe api key & secret key for add new plan.'));
-            }
-            else
-            {
+            $validation                 = [];
+            $validation['name']         = 'required|unique:plans';
+            $validation['price']        = 'required|numeric|min:0';
+            $validation['duration']     = 'required|nullable';
+            $validation['max_users']    = 'required|numeric';
+            $validation['max_clients']  = 'required|numeric';
+            $validation['max_projects'] = 'required|numeric';
+            $request->validate($validation);
+            $post = $request->all();
 
-                $validation                 = [];
-                $validation['name']         = 'required|unique:plans';
-                $validation['price']        = 'required|numeric|min:0';
-                $validation['duration']     = 'required|nullable';
-                $validation['max_users']    = 'required|numeric';
-                $validation['max_clients']  = 'required|numeric';
-                $validation['max_projects'] = 'required|numeric';
-                $request->validate($validation);
-                $post = $request->all();
+            SubscriptionPlan::create($post);
 
-                if(SubscriptionPlan::create($post))
-                {
-                    return redirect()->back()->with('success', __('SubscriptionPlan Successfully created.'));
-                }
-                else
-                {
-                    return redirect()->back()->with('error', __('Something is wrong.'));
-                }
-            }
+            return redirect()->back()->with('success', __('Subscription Plan successfully created.'));
         }
         else
         {
@@ -95,40 +82,28 @@ class SubscriptionPlansController extends Controller
     {
         if(\Auth::user()->type == 'super admin')
         {
-            if(empty(env('STRIPE_KEY')) || empty(env('STRIPE_SECRET')))
+            $plan = SubscriptionPlan::find($plan_id);
+            if(!empty($plan))
             {
-                return redirect()->back()->with('error', __('Please set stripe api key & secret key to add new plan.'));
+                $validation                 = [];
+                $validation['name']         = 'required|unique:plans,name,' . $plan_id;
+                $validation['price']        = 'required|numeric|min:0';
+                $validation['duration']     = 'required|nullable';
+                $validation['max_users']    = 'required|numeric';
+                $validation['max_clients']  = 'required|numeric';
+                $validation['max_projects'] = 'required|numeric';
+
+                $request->validate($validation);
+
+                $post = $request->all();
+
+                $plan->update($post);
+                
+                return redirect()->back()->with('success', __('Subscription Plan successfully updated.'));
             }
             else
             {
-                $plan = SubscriptionPlan::find($plan_id);
-                if(!empty($plan))
-                {
-                    $validation                 = [];
-                    $validation['name']         = 'required|unique:plans,name,' . $plan_id;
-                    $validation['price']        = 'required|numeric|min:0';
-                    $validation['duration']     = 'required|nullable';
-                    $validation['max_users']    = 'required|numeric';
-                    $validation['max_clients']  = 'required|numeric';
-                    $validation['max_projects'] = 'required|numeric';
-
-                    $request->validate($validation);
-
-                    $post = $request->all();
-
-                    if($plan->update($post))
-                    {
-                        return redirect()->back()->with('success', __('SubscriptionPlan Successfully updated.'));
-                    }
-                    else
-                    {
-                        return redirect()->back()->with('error', __('Something is wrong.'));
-                    }
-                }
-                else
-                {
-                    return redirect()->back()->with('error', __('SubscriptionPlan not found.'));
-                }
+                return redirect()->back()->with('error', __('Subscription Plan not found.'));
             }
         }
         else
@@ -136,31 +111,5 @@ class SubscriptionPlansController extends Controller
             return redirect()->back()->with('error', __('Permission denied.'));
         }
 
-    }
-
-
-    public function userPlan(Request $request)
-    {
-
-        $planID  = \Illuminate\Support\Facades\Crypt::decrypt($request->code);
-        $plan    = SubscriptionPlan::find($planID);
-
-        if($plan)
-        {
-            if($plan->price <= 0)
-            {
-                \Auth::user()->assignPlan($plan->id);
-
-                return redirect()->route('plans.index')->with('success', __('SubscriptionPlan Successfully activated.'));
-            }
-            else
-            {
-                return redirect()->back()->with('error', __('Something is wrong.'));
-            }
-        }
-        else
-        {
-            return redirect()->back()->with('error', __('SubscriptionPlan not found.'));
-        }
     }
 }
