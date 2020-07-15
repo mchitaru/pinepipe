67552fabeb570a485aea506134fa8bcba065e140
corsair <marius.chitaru@gmail.com>
2020-07-15 08:19:41 +0300

    fix project task board

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -92,16 +92,16 @@ $(function() {
                       <i class="material-icons">expand_more</i>
                     </button>
                     <div class="dropdown-menu dropdown-menu-right">
-                        @if(Gate::check('edit project') || Gate::check('delete project'))
-                        @can('edit project')
-                            <a class="dropdown-item" href="{{ route('projects.edit', $project->id) }}" data-remote="true" data-type="text">
-                                {{__('Edit Project')}}
-                            </a>
-                        @endcan
-                        <div class="dropdown-divider"></div>
                         @can('view task')
                             <a class="dropdown-item" href="{{ route('tasks.board', $project->id) }}" data-title="{{__('Task Board')}}">
                                 {{__('Task Board')}}
+                            </a>
+                        @endcan
+                        @if(Gate::check('edit project') || Gate::check('delete project'))
+                        <div class="dropdown-divider"></div>
+                        @can('edit project')
+                            <a class="dropdown-item" href="{{ route('projects.edit', $project->id) }}" data-remote="true" data-type="text">
+                                {{__('Edit Project')}}
                             </a>
                         @endcan
                         <div class="dropdown-divider"></div>
