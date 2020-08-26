bb59dd2fb65f4e87cc83d30c1b956ab70c2df1f5
corsair <marius.chitaru@gmail.com>
2020-08-26 13:36:16 +0300

    fix client delete

--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -158,6 +158,11 @@ class ClientsController extends Controller
 
         $client->delete();
 
+        if(URL::previous() == route('clients.show', $client)){
+
+            return Redirect::to(route('clients.index'))->with('success', __('Client successfully deleted'));
+        }
+
         return Redirect::to(URL::previous())->with('success', __('Client successfully deleted.'));
     }
 
