a173e512b4d2cf2db02d66dcb070c22ee6dc8487
corsair <marius.chitaru@gmail.com>
2020-09-24 14:36:43 +0300

    google conversion

--- resources/views/partials/app/head.blade.php
+++ resources/views/partials/app/head.blade.php
@@ -32,6 +32,16 @@
     @if ($message = Session::get('checkout'))
         <!-- Event snippet for Purchase conversion page --> 
         <script>gtag('event', 'conversion', { 'send_to': 'AW-591630102/_DB-CMTt794BEJaejpoC', 'transaction_id':'' });</script>
+        <!-- Google Analytics -->
+        <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+        ga('create', 'UA-165597316-1', 'auto');
+        ga('send', 'event', 'Purchase', 'purchase', 'purchase');
+        </script>
+        <!-- End Google Analytics -->        
         <!-- Facebook Pixel Code -->
         <script>
             !function(f,b,e,v,n,t,s)
