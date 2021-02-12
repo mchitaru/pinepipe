d138928417fef36907194b8fcc7141f8e212085c
corsair <marius.chitaru@gmail.com>
2021-02-12 17:43:33 +0200

    mail video

--- resources/views/mail/welcome.blade.php
+++ resources/views/mail/welcome.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 <p>{{__('Hi')}} {{$user->name}},</p><br>
 <p>{{__('You just took your first step towards organizing your business process.')}}</p>
-<p>{{__('To get started with Pinepipe, you can watch a short')}} <a href="https://youtu.be/Bab_HvQbT9I">{{__('Demo Video')}}</a></p>
+<p>{{__('To get started with Pinepipe, you can watch a short')}} <a href="https://youtu.be/{{__('Bab_HvQbT9I')}}">{{__('Demo Video')}}</a></p>
 
 @component('mail::button', ['url' => route('home')])
 {{__('Take me to my account')}}
