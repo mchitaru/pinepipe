d5887d6db1a8c83496ce069cc8604a6232102664
corsair <marius.chitaru@gmail.com>
2021-04-14 08:49:29 +0300

    fix user limit

--- app/User.php
+++ app/User.php
@@ -729,7 +729,7 @@ class User extends Authenticatable implements MustVerifyEmail, HasMedia, HasLoca
 
         $total_projects = $this->companyProjects()->count();
 
-        return $total_projects < $max_projects;
+        return $total_projects <= $max_projects;
     }
 
     public function checkClientLimit()
@@ -746,7 +746,7 @@ class User extends Authenticatable implements MustVerifyEmail, HasMedia, HasLoca
 
         $total_clients = $this->companyClients()->count();
 
-        return $total_clients < $max_clients;
+        return $total_clients <= $max_clients;
     }
 
     public function checkUserLimit()
@@ -763,7 +763,7 @@ class User extends Authenticatable implements MustVerifyEmail, HasMedia, HasLoca
 
         $total_users = $this->companyStaff()->count() + $this->collaborators->count();
 
-        return $total_users < $max_users;
+        return $total_users <= $max_users;
     }
 
     public function initCompanyDefaults()
